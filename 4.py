bc658ead10d05029ecb55955477e85c1fcd4617b
Rubi Kina <rkina@at-e1.21-school.ru>
2020-01-22 16:12:27 +0300

    add 4

---
+++ 4.py
@@ -0,0 +1,12 @@
+#-*-coding: utf-8-*-
+summ = 0
+count = 0
+for i in range(1000,10000):
+    while i > 0:
+        summ += i % 10
+        i //= 10
+    if summ * 77 == i:
+        print(i)
+        count += 1
+if count == 0:
+    print("Числа не найдены!")
